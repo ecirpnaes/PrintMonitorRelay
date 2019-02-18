baea5a7fd30e05ad1778837f412ee90441f19615
Sean Price <sean.price@agisnetwork.com>
2019-02-18 09:26:46 -0500

    Better checking of Print jobs

--- PrintMonitorRelay/PrintMonitorRelay.cs
+++ PrintMonitorRelay/PrintMonitorRelay.cs
@@ -1,11 +1,9 @@
 ﻿#region
 
 using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Management;
 using System.Net.Http;
-using System.Printing;
 using System.ServiceProcess;
 using System.Timers;
 using PrintMonitorRelay.Settings;
@@ -60,28 +58,64 @@ namespace PrintMonitorRelay
 
         private static void CheckPrinterName()
         {
-            try
-            {
-                var printQueues = new PrintServer().GetPrintQueues();
-                foreach (var printerSettings in AppSettings.PrinterSettings)
-                {
-                    var printQueue = printQueues.FirstOrDefault(pq => pq.Name == printerSettings.PrinterName);
-                    if (printQueue == null) continue;
-                    if (!printQueue.IsProcessing && !printQueue.IsPrinting && printQueue.NumberOfJobs <= 0) continue;
+            // https://www.codeproject.com/Articles/6592/A-simple-approach-for-controlling-print-jobs-using
+            var printJobCollection = new ManagementObjectSearcher("SELECT * FROM Win32_PrintJob").Get();
+            if (printJobCollection.Count == 0) return;
 
-                    PingRelay(printerSettings.IpOn);
-
-                    _timer = new Timer {Interval = TimeSpan.FromSeconds(printerSettings.Duration).TotalMilliseconds, AutoReset = false};
-                    _timer.Elapsed += (sender, e) => TimerElasped(sender, e, printerSettings);
-                    _timer.Start();
-                    break;
-                }
-            }
-            catch (Exception ex)
+            foreach (var obj in printJobCollection)
             {
-                Debug.WriteLine(ex.Message);
+                // Get the name of the printer from the print job
+                var printJob = (ManagementObject) obj;
+                var printerName = GetPrinterName(printJob.Properties["Name"].Value);
+                if (printerName.IsEmpty()) continue;
+
+                // Search for this printer name in our list of App settings
+                var appSetting = AppSettings.PrinterSettings.FirstOrDefault(pq => pq.PrinterName == printerName);
+                if (appSetting == null) continue;
+
+                // If we found it, ping the relay (turn it on) 
+                PingRelay(appSetting.IpOn);
+
+                // Set a timer for our desired duration to turn it off. Note-We only want to raise the timer event once
+                _timer = new Timer {Interval = TimeSpan.FromSeconds(appSetting.Duration).TotalMilliseconds, AutoReset = false};
+                _timer.Elapsed += (sender, e) => TimerElasped(sender, e, appSetting);
+                _timer.Start();
+                break;
             }
         }
+
+        private static string GetPrinterName(object jobName)
+        {
+            if (jobName == null || jobName.ToString().IsEmpty()) return string.Empty;
+
+            //Job name would be of the format [Printer name], [Job ID]
+            return jobName.ToString().Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries)[0];
+        }
+
+        //private static void CheckPrinterName()
+        //{
+        //    try
+        //    {
+        //        var printQueues = new PrintServer().GetPrintQueues();
+        //        foreach (var printerSettings in AppSettings.PrinterSettings)
+        //        {
+        //            var printQueue = printQueues.FirstOrDefault(pq => pq.Name == printerSettings.PrinterName);
+        //            if (printQueue == null) continue;
+        //            if (!printQueue.IsProcessing && !printQueue.IsPrinting && printQueue.NumberOfJobs <= 0) continue;
+
+        //            PingRelay(printerSettings.IpOn);
+
+        //            _timer = new Timer {Interval = TimeSpan.FromSeconds(printerSettings.Duration).TotalMilliseconds, AutoReset = false};
+        //            _timer.Elapsed += (sender, e) => TimerElasped(sender, e, printerSettings);
+        //            _timer.Start();
+        //            break;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Debug.WriteLine(ex.Message);
+        //    }
+        //}
 
         private static void TimerElasped(object sender, ElapsedEventArgs e, AppSettings.PrinterSetting printerSettings)
         {
---
+++ PrintMonitorRelay/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace PrintMonitorRelay
+{
+    public static class StringExtensions
+    {
+        public static bool IsEmpty(object input)
+        {
+            return input == null || IsEmpty(input.ToString());
+        }
+
+        public static bool IsEmpty(this string input)
+        {
+            return string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input);
+        }
+    }
+}
